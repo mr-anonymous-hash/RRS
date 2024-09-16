904e4e23eeceab8ab6d27357be35867fdff16a62
anonymous23 <sibivarmas23@gmail.com>
2024-09-16 09:51:18 +0000

    crud updated

--- RRS_backend/src/crud/users_crud.js
+++ RRS_backend/src/crud/users_crud.js
@@ -2,10 +2,10 @@ const {User} = require('./../models');
 const db_factory = require('./../utils/db_factory');
 
 const getAllUsers = async() => await db_factory.getAllRecords(User)
-const getUserById = async() => await db_factory.getRecordById(User)
-const createUser = async() => await db_factory.createRecord(User)
-const updateUser = async() => await db_factory.updateRecord(User)
-const deleteUser = async ()=> await db_factory.deleteRecord(User)
+const getUserById = async(id) => await db_factory.getRecordById(User, id)
+const createUser = async(data) => await db_factory.createRecord(User, data)
+const updateUser = async(id) => await db_factory.updateRecord(User, id)
+const deleteUser = async (id)=> await db_factory.deleteRecord(User, id)
 
 module.exports = {
     getAllUsers,getUserById,createUser,updateUser,deleteUser
