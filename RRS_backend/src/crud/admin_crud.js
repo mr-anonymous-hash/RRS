76be16634e515db4da7a840fa167549629e0a1dc
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-16 12:46:10 +0530

    admin crud added

--- RRS_backend/src/crud/admin_crud.js
+++ RRS_backend/src/crud/admin_crud.js
@@ -1,12 +1,12 @@
 const {Admin} = require('../models');
 const db_factory = require('../utils/db_factory');
 
-const getAllUsers = async() => await db_factory.getAllRecords(Admin)
-const getUserById = async() => await db_factory.getRecordById(Admin)
-const createUser = async() => await db_factory.createRecord(Admin)
-const updateUser = async() => await db_factory.updateRecord(Admin)
-const deleteUser = async ()=> await db_factory.deleteRecord(Admin)
+const getAllAdmin = async() => await db_factory.getAllRecords(Admin)
+const getAdminById = async() => await db_factory.getRecordById(Admin)
+const createAdmin = async() => await db_factory.createRecord(Admin)
+const updateAdmin = async() => await db_factory.updateRecord(Admin)
+const deleteAdmin = async ()=> await db_factory.deleteRecord(Admin)
 
 module.exports = {
-    getAllUsers,getUserById,createUser,updateUser,deleteUser
+    getAllAdmin,getAdminById,createAdmin,updateAdmin,deleteAdmin
 }
