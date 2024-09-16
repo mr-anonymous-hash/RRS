5638b5ecc9ed951745761c5d15507d4321847ec6
anonymous23 <sibivarmas23@gmail.com>
2024-09-16 09:50:16 +0000

    route path added

--- RRS_backend/index.js
+++ RRS_backend/index.js
@@ -2,5 +2,5 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-const db = require('./src/models')
+app.use('/api', require('./src/routes/index'))
 app.listen(port, console.log(`Listening on http://localhost:${port}`))
