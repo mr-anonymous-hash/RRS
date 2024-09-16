4395051acefcadc8aa5f336de70a57d95ea282f3
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-16 12:46:29 +0530

    db_factory added

---
+++ RRS_backend/src/utils/db_factory.js
@@ -0,0 +1,59 @@
+const { where } = require("sequelize");
+
+const getAllRecords = async(model) =>{
+    try{
+        const records = await model.findAll();
+        return records 
+    }
+    catch(err){
+        console.log(`Error while Fetching Users: ${err}`)
+    }
+}
+
+const getRecordById = async(model, id) =>{
+    try{
+        const record = await model.findByPk(id);
+        return record 
+    }
+    catch(err){
+        console.log(`Error while Fetching Users: ${err}`)
+    }
+}
+
+const createRecord = async(model, data) =>{
+    try{
+        const records = await model.create(data);
+        return records 
+    }
+    catch(err){
+        console.log(`Error while Fetching Users: ${err}`)
+    }
+}
+
+const updateRecord = async(model, data, id) => {
+    try{
+        const records = await model.update(
+            {fields: data},
+            {where: {id: id}}
+        );
+        return records 
+    }
+    catch(err){
+        console.log(`Error while Fetching Users: ${err}`)
+    }
+}
+
+const deleteRecord = async(model, id) => {
+    try{
+        const records = await model.destroy({where:{id: id}});
+        return records 
+    }
+    catch(err){
+        console.log(`Error while Fetching Users: ${err}`)
+    }
+}
+
+module.exports = {
+    getAllRecords,getRecordById,createRecord,
+    updateRecord,deleteRecord
+}
