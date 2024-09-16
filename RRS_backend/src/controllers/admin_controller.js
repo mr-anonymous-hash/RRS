a0d3c84dc406c3a9c0b840da32bd8ca853d05e72
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-16 21:43:05 +0530

    admin controller created

---
+++ RRS_backend/src/controllers/admin_controller.js
@@ -0,0 +1,27 @@
+const { Admin } = require('../models');
+const admin_crud = require('./../crud/admin_crud');
+
+const getAllAdmin = async(req, res) => {
+    const admin = await admin_crud.getAllAdmin()
+    if(admin){
+        res.status(200).send(admin)
+    }
+    else{
+        res.status(404).send('unable to fetch')
+    }
+}
+
+const getAdminById = async(req, res) => {
+    const id = req.params
+    const admin = await admin_crud.getAdminById(id)
+    if(admin){
+        res.status(200).send(admin)
+    }
+    else{
+        res.status(404).send('Admin not found')
+    }
+}
+
+module.exports = {
+    getAllAdmin,getAdminById,
+}
