3c4d5f4b864e097a44cdbbb4a6ff1d174202a7e7
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-16 21:42:01 +0530

    parameter issue fix

--- RRS_backend/src/crud/hotel_crud.js
+++ RRS_backend/src/crud/hotel_crud.js
@@ -2,10 +2,10 @@ const {Hotel} = require('../models');
 const db_factory = require('../utils/db_factory');
 
 const getAllHotels = async() => await db_factory.getAllRecords(Hotel)
-const getHotelById = async() => await db_factory.getRecordById(Hotel)
-const createHotel = async() => await db_factory.createRecord(Hotel)
-const updateHotel = async() => await db_factory.updateRecord(Hotel)
-const deleteHotel = async ()=> await db_factory.deleteRecord(Hotel)
+const getHotelById = async(id) => await db_factory.getRecordById(Hotel, id)
+const createHotel = async(data) => await db_factory.createRecord(Hotel, data)
+const updateHotel = async(id, data,) => await db_factory.updateRecord(Hotel, id, data)
+const deleteHotel = async (id)=> await db_factory.deleteRecord(Hotel, id)
 
 module.exports = {
     getAllHotels,getHotelById,createHotel,updateHotel,deleteHotel
--- RRS_backend/src/crud/users_crud.js
+++ RRS_backend/src/crud/users_crud.js
@@ -4,7 +4,7 @@ const db_factory = require('./../utils/db_factory');
 const getAllUsers = async() => await db_factory.getAllRecords(User)
 const getUserById = async(id) => await db_factory.getRecordById(User, id)
 const createUser = async(data) => await db_factory.createRecord(User, data)
-const updateUser = async(id) => await db_factory.updateRecord(User, id)
+const updateUser = async(id, data) => await db_factory.updateRecord(User, id, data)
 const deleteUser = async (id)=> await db_factory.deleteRecord(User, id)
 
 module.exports = {
