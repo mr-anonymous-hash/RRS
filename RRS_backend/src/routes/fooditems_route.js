a3ef82ec340084078b5782fe8a20f6de0bc67505
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-17 10:42:56 +0530

    route path for  food items and reservation modifiy

--- RRS_backend/src/routes/fooditems_route.js
+++ RRS_backend/src/routes/fooditems_route.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const fooditems_controller = require('./../controllers/fooditems_controller')
 
-router.get('/',()=>{[]})
-router.get('/:id',()=>{[]})
-router.post('/',()=>{[]})
-router.put('/:id',()=>{[]})
-router.delete('/:id',()=>{[]})
+router.get('/', fooditems_controller.getAllFoodItems)
+router.get('/:id', fooditems_controller.getFoodItemsById)
+router.post('/', fooditems_controller.createFoodItem)
+router.put('/:id', fooditems_controller.updateFoodItem)
+router.delete('/:id', fooditems_controller.deleteFoodItem)
 
 module.exports = router
--- RRS_backend/src/routes/reservations_route.js
+++ RRS_backend/src/routes/reservations_route.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
+const reservations_controller = require('./../controllers/reservation_controller')
 
-router.get('/',()=>{[]})
-router.get('/:id',()=>{[]})
-router.post('/',()=>{[]})
-router.put('/:id',()=>{[]})
-router.delete('/:id',()=>{[]})
+router.get('/', reservations_controller.getAllReservations)
+router.get('/:id', reservations_controller.getReservationsById)
+router.post('/', reservations_controller.createReservation)
+router.delete('/:id', reservations_controller.deleteReservation)
 
 module.exports = router
