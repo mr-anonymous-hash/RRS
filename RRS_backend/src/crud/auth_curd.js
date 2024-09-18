b6976af097e6906f2db03b0eecdb3a3cbcd007de
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 18:00:55 +0530

    modify crud for signup

--- RRS_backend/src/crud/auth_curd.js
+++ RRS_backend/src/crud/auth_curd.js
@@ -1,9 +1,11 @@
 const { where } = require('sequelize')
 const {User} = require('./../models')
+const db_factory = require('./../utils/db_factory')
+const signup = async(data) => await db_factory.createRecord(User, data)
 
 const login = async (email) => {
     const user = await User.findOne({where:{email}});
     return user
 }
 
-module.exports = {login}
+module.exports = {login,signup}
--- RRS_backend/src/crud/users_crud.js
+++ RRS_backend/src/crud/users_crud.js
@@ -3,10 +3,9 @@ const db_factory = require('./../utils/db_factory');
 
 const getAllUsers = async() => await db_factory.getAllRecords(User)
 const getUserById = async(id) => await db_factory.getRecordById(User, id)
-const createUser = async(data) => await db_factory.createRecord(User, data)
 const updateUser = async(id, data) => await db_factory.updateRecord(User, id, data)
 const deleteUser = async (id)=> await db_factory.deleteRecord(User, id)
 
 module.exports = {
-    getAllUsers,getUserById,createUser,updateUser,deleteUser
+    getAllUsers,getUserById,updateUser,deleteUser
 }
