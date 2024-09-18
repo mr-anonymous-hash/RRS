c69a18e6290637424239fa49e06999923f3c4846
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 22:05:02 +0530

    endpoint added

--- rrs_frontend/src/components/SideNav.jsx
+++ rrs_frontend/src/components/SideNav.jsx
@@ -1,11 +1,20 @@
+import { Router, useRouter } from 'next/router'
 import React from 'react'
+import './../app/globals.css'
 
 const SideNav = () => {
+  const router = useRouter();
+  const logout = () => {
+    localStorage.removeItem('token')
+    router.push('/login')
+  }
+ 
   return (
     <div className='w-40 h-auto text-black ' >
-        <div>Home</div>
-        <div>Food Items</div>
-        <div>Settings</div>
+        <div><a onClick={()=>router.push('/home')} className='cursor-pointer'>Home</a></div>
+        <div><a onClick={()=>router.push('/foodItems')} className='cursor-pointer'>Food Items</a></div>
+        <div><a onClick={()=>router.push('/settings')} className='cursor-pointer'>Settings</a></div>
+        <div><a onClick={logout} className='cursor-pointer'>Logout</a></div>
     </div>
   )
 }
