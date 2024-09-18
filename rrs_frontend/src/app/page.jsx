f214029ac8e29a6aec6128c81294aa468fb92023
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 18:03:11 +0530

    endpoint change

--- rrs_frontend/src/app/page.jsx
+++ rrs_frontend/src/app/page.jsx
@@ -3,7 +3,7 @@ import React, {useEffect} from "react";
 
 export default function Home() {
   useEffect(()=>{
-    window.location.href = '/signin'
+    window.location.href = '/signup'
 },[])
   return (
     <div >
