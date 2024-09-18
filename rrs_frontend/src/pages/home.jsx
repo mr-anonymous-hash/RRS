a4b297bf647a9f49360f1eab9d026f8ba7962814
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 22:07:03 +0530

    use token to athenticate users

--- rrs_frontend/src/pages/home.jsx
+++ rrs_frontend/src/pages/home.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './../app/globals.css'
 import SideNav from './../components/SideNav'
+import { useRouter } from 'next/router'
 
 const Home = () => {
+  const router = useRouter();
+  const [username, setUserName] = useState('')
+  const [role, setRole] = useState('')
+
+  useEffect(()=>{
+    const token = localStorage.getItem('token')
+    const user = localStorage.getItem('user')
+  
+    if(!token){
+      router.push('/login')
+    }
+    if(user){
+      try{
+        const userObject = JSON.parse(user)
+        setUserName(userObject.name)
+        setRole(userObject.role)
+        if(!role == 'true'){
+          router.push('/users/home')
+        }
+      }
+      catch(error){
+        console.error(`Error prasing user data:${error}`)
+      }
+    }
+
+  },[router])
+
   return (
     <div className='flex'>
       <div>
@@ -10,7 +38,7 @@ const Home = () => {
       </div>
       <div>
         <div className='bg-blue-400 h-48 w-full rounded-md'>
-          <h1 className='font-extrabold text-2xl'>Hello, Admin</h1>
+          <h1 className='font-extrabold text-2xl'>Hello, { username || 'Admin'}</h1>
           <p>Hotel Annaporanas, Singanallore</p>
         </div>
         <div>
