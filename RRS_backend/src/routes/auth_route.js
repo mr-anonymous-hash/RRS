5d104430837602af20156dbd3c065be13d89c879
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 18:01:42 +0530

    modify route path for signup

--- RRS_backend/src/routes/auth_route.js
+++ RRS_backend/src/routes/auth_route.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const auth_controller = require('./../controllers/auth_controller')
 
-router.post('/', auth_controller.login)  
-
+router.post('/signup', auth_controller.signup)
+router.post('/login', auth_controller.login)
+  
 module.exports = router
--- RRS_backend/src/routes/users_route.js
+++ RRS_backend/src/routes/users_route.js
@@ -3,7 +3,6 @@ const users_controller = require('./../controllers/users_controller')
 
 router.get('/', users_controller.getAllUsers)
 router.get('/:id', users_controller.getUserById)
-router.post('/', users_controller.createUser)
 router.put('/:id', users_controller.updateUser)
 router.delete('/:id', users_controller.deleteUser)
 
