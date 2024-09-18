6ab822ae7f5373b6f0c403cf69e0889f6ea56a43
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-18 17:59:02 +0530

    cors install

--- RRS_backend/index.js
+++ RRS_backend/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { json } = require('sequelize');
+const cors = require('cors');
 const app = express();
-const port = 8080;
+const port = 8000;
 
+app.use(cors());
 app.use(express.json())
 app.use('/api', require('./src/routes/'))
 app.listen(port, console.log(`Listening on http://localhost:${port}`))
