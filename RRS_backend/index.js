3aa825ad8d33ffc107ba83da550f2540fe3f6d03
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-20 14:21:45 +0530

    remove unused import

--- RRS_backend/index.js
+++ RRS_backend/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { json } = require('sequelize');
 const cors = require('cors');
 const app = express();
 const port = 8000;
 
 app.use(cors());
 app.use(express.json())
+
 app.use('/api', require('./src/routes/'))
+
 app.listen(port, console.log(`Listening on http://localhost:${port}`))
