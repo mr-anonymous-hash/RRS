0b11a32dda8bb037f9f4d8bc4b56df3fdb862faa
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-24 09:24:56 +0530

    icons added

--- rrs_frontend/src/components/SideNav.jsx
+++ rrs_frontend/src/components/SideNav.jsx
@@ -1,6 +1,9 @@
 import { Router, useRouter } from 'next/router'
 import React from 'react'
 import './../app/globals.css'
+import { TiHomeOutline } from "react-icons/ti";
+import { IoSettingsOutline } from "react-icons/io5";
+import { MdLogout } from "react-icons/md";
 
 const SideNav = () => {
   const router = useRouter();
@@ -14,21 +17,21 @@ const SideNav = () => {
     <div className='flex flex-col space-y-2'>
       <a 
         onClick={() => router.push('/home')} 
-        className='cursor-pointer hover:bg-gray-700 p-2 rounded transition duration-200'
+        className='cursor-pointer hover:bg-gray-700 p-2 rounded transition duration-200 flex items-center'
       >
-        Home
+       <TiHomeOutline className='text-xl'/> Home
       </a>
       <a 
         onClick={() => router.push('/settings')} 
-        className='cursor-pointer hover:bg-gray-700 p-2 rounded transition duration-200'
+        className='cursor-pointer hover:bg-gray-700 p-2 rounded transition duration-200 flex items-center'
       >
-        Settings
+        <IoSettingsOutline className='text-xl'/> Settings
       </a>
       <a 
         onClick={logout} 
-        className='cursor-pointer hover:bg-red-500 p-2 rounded transition duration-200'
+        className='cursor-pointer hover:bg-red-500 p-2 rounded transition duration-200 flex items-center'
       >
-        Logout
+        <MdLogout className='text-xl'/>Logout
       </a>
     </div>
   </div>
