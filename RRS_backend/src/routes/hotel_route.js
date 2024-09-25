25cb682212e6be1c9f1144c9e4a94d2b6656f1f8
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-25 09:31:10 +0530

    hotel route modified

--- RRS_backend/src/routes/hotel_route.js
+++ RRS_backend/src/routes/hotel_route.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
-const hotel_controller = require('./../controllers/hotel_controller')
+    const router = require('express').Router();
+    const hotel_controller = require('./../controllers/hotel_controller')
 
-router.get('/', hotel_controller.getAllHotels)
-router.get('/:id', hotel_controller.getHotelById)
-router.get('/admin/:id', hotel_controller.getHotelsByAdminId)
-router.post('/', hotel_controller.createHotel)
-router.put('/:id', hotel_controller.updateHotel)
-router.delete('/:id', hotel_controller.deleteHotel)
+    router.get('/', hotel_controller.getAllHotels)
+    router.get('/:id', hotel_controller.getHotelById)
+    router.get('/admin/:id', hotel_controller.getHotelsByAdminId)
+    router.post('/', hotel_controller.createHotel)
+    router.put('/:id', hotel_controller.updateHotel)
+    router.delete('/:id', hotel_controller.deleteHotel)
 
-module.exports = router
+    module.exports = router
