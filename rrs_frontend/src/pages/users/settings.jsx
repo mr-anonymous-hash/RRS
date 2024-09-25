26b66b798ec6e14c325155bc411b81fadb5d1931
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-25 09:34:16 +0530

    settings for user implemented

---
+++ rrs_frontend/src/pages/users/settings.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import SideNav from './../../components/SideNav'
+
+const settings = () => {
+  return (
+    <div>
+      <div>
+        <SideNav/>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default settings
