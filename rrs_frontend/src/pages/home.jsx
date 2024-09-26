3daaef7c695bca165929898104e6c986f25d42e0
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-26 12:50:10 +0530

    loading ui fix in admin home

--- rrs_frontend/src/pages/home.jsx
+++ rrs_frontend/src/pages/home.jsx
@@ -61,7 +61,6 @@ const Home = () => {
     }
   };
   
-
   useEffect(()=>{
     fetchHotels()
   },[])
@@ -70,7 +69,7 @@ const Home = () => {
     <div className="flex items-center justify-center min-h-screen">
       <div className="flex items-center space-x-2">
         <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-blue-500"></div>
-        <div className="text-lg font-semibold">Loading...</div>
+        <div className="text-lg font-semibold text-slate-800">Loading...</div>
       </div>
     </div>
   );
