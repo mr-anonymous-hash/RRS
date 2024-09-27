901e12177b01b223b0a60a6531183163cc5331f6
mr-anonymous-hash <sibivarma11@gmail.com>
2024-09-27 17:00:05 +0530

    endpoint change

--- rrs_frontend/src/components/FoodSelection.jsx
+++ rrs_frontend/src/components/FoodSelection.jsx
@@ -1,23 +1,62 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-const FoodSelection = ({ foodItems, onFoodSelect }) => {
+const FoodSelection = ({ hotelId, onFoodSelect }) => {
   const [selectedFood, setSelectedFood] = useState([]);
+  const [foodItems, setFoodItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchFoodItems = async () => {
+    const token = localStorage.getItem('token');
+    setIsLoading(true);
+    try {
+      const res = await fetch(`http://localhost:8000/api/fooditems/hotel/${hotelId}`, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        }
+      });
+      if (res.ok) {
+        const data = await res.json();
+        setFoodItems(data);
+      } else {
+        throw new Error('Failed to fetch food items');
+      }
+    } catch (error) {
+      console.error(`Error while fetching food items: ${error}`);
+      setError('Failed to load food items. Please try again.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchFoodItems();
+  }, []);
 
   const handleFoodSelection = (foodItem) => {
     setSelectedFood(prev => {
       const newSelection = prev.some(item => item.id === foodItem.id)
         ? prev.filter(item => item.id !== foodItem.id)
         : [...prev, foodItem];
-      
       onFoodSelect(newSelection);
       return newSelection;
     });
   };
 
+  if (isLoading) {
+    return <div className="text-center">Loading food items...</div>;
+  }
+
+  if (error) {
+    return <div className="text-center text-red-500">{error}</div>;
+  }
+
   return (
     <div className="mt-4 p-4 border rounded">
-      <h3 className="text-xl mb-2 text-black">Select Food Items</h3>
-      <div className="grid grid-cols-3 gap-4">
+      <h3 className="text-xl mb-2 text-slate-800">Select Food Items</h3>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 text-slate-800">
         {foodItems.map((item) => (
           <div
             key={item.id}
@@ -37,7 +76,7 @@ const FoodSelection = ({ foodItems, onFoodSelect }) => {
         <h4 className="text-lg text-slate-800 font-bold">Selected Items:</h4>
         <ul>
           {selectedFood.map(item => (
-            <li key={item.id}>{item.food_name} - ${item.price}</li>
+            <li key={item.id} className='text-slate-500'>{item.food_name} - ${item.price}</li>
           ))}
         </ul>
         <p className="font-bold text-slate-500 mt-2">
