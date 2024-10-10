8f2d740fb3bfab8a33eb13de3d4c986b2e94a28f
mr-anonymous-hash <sibivarma11@gmail.com>
2024-10-10 18:29:22 +0530

    update booking button added

--- rrs_frontend/src/pages/settings.jsx
+++ rrs_frontend/src/pages/settings.jsx
@@ -11,11 +11,17 @@ const settings = () => {
         <SideNav />
       </div>
       <div>
-        <div className='text-white text-center bg-slate-500 
-        ml-10 mt-10 rounded-lg min-h-60 min-w-60 cursor-pointer
+      <div className='text-white text-center bg-slate-500 
+        ml-10 mt-10 rounded-lg min-h-28 min-w-60 cursor-pointer
         hover:bg-slate-300 hover:text-slate-500 flex items-center justify-center'
         onClick={()=>router.push('/add_hotel')}>
           <p className='text-xl'>Add Hotels</p>  
+        </div>
+        <div className='text-white text-center bg-slate-500 
+        ml-10 mt-10 rounded-lg min-h-28 min-w-60 cursor-pointer
+        hover:bg-slate-300 hover:text-slate-500 flex items-center justify-center'
+        onClick={()=>router.push('/add_hotel')}>
+          <p className='text-xl'>Update Hotels</p>  
         </div>
       </div>
     </div>
